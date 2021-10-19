68299d009be09fe182c93ffbbbdc3747d017165a
ericasaline <ericasaline@gmail.com>
2021-10-19 10:20:50 -0300

    estrutura finalizada

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const pixelsArray = []
 const width = 10
 const height = 10
+const fireColorsPalette = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}]
 
 
 function start() {
     fireStructure()
+    createFire()
     renderFire()
+
+    setInterval(firePropagation, 50)
 }
 
 function fireStructure() {
@@ -17,7 +21,15 @@ function fireStructure() {
 }
 
 function firePropagation() {
+    for(let column=0; column<width; column++){
+        for(let row=0; row<height; row++){
+            const pixelIndex = column + width * row
 
+            updateFireIntensity(pixelIndex)
+        }
+    }
+
+    renderFire()
 }
 
 function renderFire() {
@@ -42,6 +54,20 @@ function renderFire() {
     html += '</table>'
 
     document.querySelector('#fireCanvas').innerHTML = html
+}
+
+function updateFireIntensity(pixelIndex) {
+    const belowPixelIndex = pixelIndex + width
+
+    if(belowPixelIndex >= width * height) {
+        return 
+    }
+
+    const decay = Math.floor(Math.random() * 3)
+    const belowPixelFireIntensity = pixelsArray[belowPixelIndex]
+    const newFireIntensity = belowPixelFireIntensity - decay >= 0 ? belowPixelFireIntensity - decay : 0 
+
+    pixelsArray[pixelIndex - decay] = newFireIntensity
 }
 
 
